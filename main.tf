2f86b30542a91fb4936cdf280af638e45f5af420
supraja-saravanan <32225146+supraja-saravanan@users.noreply.github.com>
2023-10-13 14:54:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         	   = "sctp1112"
+    bucket         	   = "sctp000"
     key              	   = "state/terraform.tfstate"
     region         	   = "us-east-1"
     encrypt        	   = true
